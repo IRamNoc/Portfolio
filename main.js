c2f0f2ec5fbd986608c0c4b47a87846770073f7c
IRamNoc <90531307+IRamNoc@users.noreply.github.com>
2024-03-28 16:02:07 +0000

    Deploying to gh-pages from @ IRamNoc/Portfolio@6146aa022f2eae4ac32e7523fd00cb7110d47b88 🚀

--- main.js
+++ main.js
@@ -12,106 +12,78 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "projectsEn", function() { return projectsEn; });
 const projectsEn = [
     {
-        id: "teamTraveler",
-        name: "Team Traveler",
-        technologies: 'Android Studio, Java, FireBase, MVP, NoSql',
-        images: [
-            {
-                title: 'Trip',
-                src: "assets/images/voyages.webp",
-            },
-            {
-                title: 'Trip',
-                src: "assets/images/voyage.webp",
-            },
-            {
-                title: 'Housing',
-                src: "assets/images/logements.webp",
-            },
-        ],
-        url: "https://play.google.com/store/apps/details?id=team.traveler.teamtraveler",
-        detail: "The application will allow you to organize group trips easily and quickly. Vote for dates, transport, accommodation or even activities. Thanks to this <a title='Download on Google Play' href='https://play.google.com/store/apps/details?id=team.traveler.teamtraveler'>application</a> no more problems with choices or disagreements.",
-        codeSourceUrl: 'https://github.com/zinedineBenkhider/team_traveler',
-        detailIsDisplayed: false,
-    },
-    {
-        id: "mautic",
-        name: "Mautic Monitoring",
-        technologies: 'Android Studio, Java, Rx_Java, Retrofit, MVP, Rest',
-        images: [
-            {
-                title: 'Marketing campaigns',
-                src: "assets/images/compagnes.jpg",
-            },
-            {
-                title: 'Campaign detail',
-                src: "assets/images/compagne-detail.jpg",
-            },
-            {
-                title: "Email performance",
-                src: "assets/images/email-detail.jpg",
-            },
-            {
-                title: "Performance of an asset",
-                src: "assets/images/asset-detail.jpg"
-            },
-        ],
-        url: '#portfolio',
-        detail: "<p>Mautic is an open source automatic marketing tool</p> <p>The application allows you to have dynamic and detailed reports, to analyze individual campaign trends.<br> A visually user-friendly dashboard gives you a unique quick view of the performance of: your digital campaigns, assets, and forms. </p> <p>The application is available in French and English.</p>",
-        codeSourceUrl: 'https://github.com/zinedineBenkhider/mautic_monitoring',
-        detailIsDisplayed: false,
-    },
-    {
-        id: "pokedex",
-        name: "Pokedex",
-        technologies: 'Java, J2EE, SpringBoot, TDD, MySQL, BootStrap, Services, Deploiment',
-        images: [
-            {
-                title: 'Pokedex',
-                src: "assets/images/pokedex.png",
-            },
-            {
-                title: 'Trainers',
-                src: "assets/images/trainers.png",
-            },
-            {
-                title: "Profile",
-                src: "assets/images/profile-trainer.png",
-            },
-        ],
-        url: '#portfolio',
-        detail: "<p>Display of the details of pokemon and their trainers.</p> <p>The two micro-service application contains: pokemon management and trainer management, which exposes a REST/JSON communication channel.<br>The 3rd WEB-UI micro-service connects to both micro-services.</p>",
-        codeSourceUrl: 'https://github.com/ifi-2019/trainer-ui-zinedineBenkhider',
-        detailIsDisplayed: false,
-    },
-    {
         id: "portfolio",
         name: "Portfolio",
         technologies: 'Angular11, Html5, CSS, TypeScript, BootStrap',
         images: [
             {
                 title: 'Portfolio',
-                src: "assets/images/portfolio.png",
+                src: "assets/images/ProjetPortfolio.png",
             },
         ],
-        url: 'https://medium.com/zinedine-projects/beautiful-cv-portfolio-72921a198640',
-        detail: "<p> CV in the form of a responsive WEB site that I put in open-source on GitHub accompanied by a publication of a <a title='See the article' href='https://medium.com/zinedine-projects/beautiful-cv-portfolio-72921a198640' target='_blank'>article</a> on medium. </p>",
-        codeSourceUrl: 'https://github.com/zinedineBenkhider/portfolio',
+        url: 'https://iramnoc.github.io/Portfolio/',
+        detail: "<p>CV sous forme de site WEB responsive que j'ai mis en open-source sur GitHub </p>",
+        codeSourceUrl: 'https://github.com/IRamNoc/Portfolio',
         detailIsDisplayed: false,
     },
     {
-        id: "riddle",
-        name: "Transfer Riddle",
-        technologies: 'Java, J2EE(Web Services), JavaScript, Html5, CSS, BootStrap',
+        id: "",
+        name: "",
+        technologies: '',
         images: [
             {
-                title: 'Riddle',
-                src: "assets/images/riddle.png",
+                title: '',
+                src: "",
             },
         ],
-        url: 'https://transfer-riddle-solver.herokuapp.com/index.html',
-        detail: "Display of the steps for solving the decanting puzzle. Given two empty tanks of capacity X and Y, how to obtain precisely T liters in one of the two tanks (T being an integer). You can fill or empty a tank , and transfer the contents from one to the other, until the source is empty or the destination full.",
-        codeSourceUrl: 'https://github.com/zinedineBenkhider/transfer_riddle_client',
+        url: '',
+        detail: "",
+        codeSourceUrl: '',
+        detailIsDisplayed: false,
+    },
+    {
+        id: "",
+        name: "",
+        technologies: '',
+        images: [
+            {
+                title: '',
+                src: "",
+            },
+        ],
+        url: '',
+        detail: "",
+        codeSourceUrl: '',
+        detailIsDisplayed: false,
+    },
+    {
+        id: "",
+        name: "",
+        technologies: '',
+        images: [
+            {
+                title: '',
+                src: "",
+            },
+        ],
+        url: '',
+        detail: "",
+        codeSourceUrl: '',
+        detailIsDisplayed: false,
+    },
+    {
+        id: "",
+        name: "",
+        technologies: '',
+        images: [
+            {
+                title: '',
+                src: "",
+            },
+        ],
+        url: '',
+        detail: "",
+        codeSourceUrl: '',
         detailIsDisplayed: false,
     },
 ];
@@ -1876,106 +1848,78 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "projectsFr", function() { return projectsFr; });
 const projectsFr = [
     {
-        id: "teamTraveler",
-        name: "Team Traveler",
-        technologies: 'Android Studio, Java, FireBase, MVP, NoSql',
-        images: [
-            {
-                title: '',
-                src: "assets/images/voyages.webp",
-            },
-            {
-                title: '',
-                src: "assets/images/voyage.webp",
-            },
-            {
-                title: '',
-                src: "assets/images/logements.webp",
-            },
-        ],
-        url: "https://play.google.com/store/apps/details?id=team.traveler.teamtraveler",
-        detail: "L'application vous permettra d'organiser des voyage en groupe facilement et rapidement. Votez pour les dates, les transports, hébergement ou même les activités. Grâce à cette <a title='Télécharger sur Google Play' href='https://play.google.com/store/apps/details?id=team.traveler.teamtraveler'>application</a> plus de problème de choix ou désaccords.",
-        codeSourceUrl: 'https://github.com/zinedineBenkhider/team_traveler',
-        detailIsDisplayed: false,
-    },
-    {
-        id: "mautic",
-        name: "Mautic Monitoring",
-        technologies: 'Android Studio, Java, Rx_Java, Retrofit, MVP, Rest',
-        images: [
-            {
-                title: 'Compagnes de marketing',
-                src: "assets/images/compagnes.jpg",
-            },
-            {
-                title: 'Détail de la compagne',
-                src: "assets/images/compagne-detail.jpg",
-            },
-            {
-                title: "Performance d'un émail",
-                src: "assets/images/email-detail.jpg",
-            },
-            {
-                title: "Performance d'un Asset",
-                src: "assets/images/asset-detail.jpg"
-            },
-        ],
-        url: '#portfolio',
-        detail: "<p>Mautic est un outil open source de marketing automatique</p> <p>L'application permet d'avoir des rapports dynamiques et détaillés, d’analyser les tendances individuels des campagnes.<br> Un tableau de bord visuellement convivial vous donne un point de vue rapide unique sur la performance de: vos campagnes numériques, assets, et formulaires. </p> <p>L'application est disponible en Français et en Anglais.</p>",
-        codeSourceUrl: 'https://github.com/zinedineBenkhider/mautic_monitoring',
-        detailIsDisplayed: false,
-    },
-    {
-        id: "pokedex",
-        name: "Pokedex",
-        technologies: 'Java, J2EE, SpringBoot, TDD, MySQL, BootStrap, Services, Deploiment',
-        images: [
-            {
-                title: 'Pokedex',
-                src: "assets/images/pokedex.png",
-            },
-            {
-                title: 'Trainers',
-                src: "assets/images/trainers.png",
-            },
-            {
-                title: "Profile",
-                src: "assets/images/profile-trainer.png",
-            },
-        ],
-        url: '#portfolio',
-        detail: "<p>Affichage du détail des pokémons et de leurs drésseurs.</p> <p>L'application contient deux micro-service: pokemon management et trainer management, qui exposent un canal de communication REST/JSON.<br>Le 3éme micro-service WEB-UI se connecte aux deux micro-service.</p>",
-        codeSourceUrl: 'https://github.com/ifi-2019/trainer-ui-zinedineBenkhider',
-        detailIsDisplayed: false,
-    },
-    {
         id: "portfolio",
         name: "Portfolio",
         technologies: 'Angular11, Html5, CSS, TypeScript, BootStrap',
         images: [
             {
                 title: 'Portfolio',
-                src: "assets/images/portfolio.png",
+                src: "assets/images/ProjetPortfolio.png",
             },
         ],
-        url: 'https://medium.com/zinedine-projects/beautiful-cv-portfolio-72921a198640',
-        detail: "<p>CV sous forme de site WEB responsive que j'ai mis en open-source sur GitHub accompagné d'une publication d'un <a title='Voir l'article' href='https://medium.com/zinedine-projects/beautiful-cv-portfolio-72921a198640' target='_blank'>article</a> sur medium. </p>",
-        codeSourceUrl: 'https://github.com/zinedineBenkhider/portfolio',
+        url: 'https://iramnoc.github.io/Portfolio/',
+        detail: "<p>CV sous forme de site WEB responsive que j'ai mis en open-source sur GitHub </p>",
+        codeSourceUrl: 'https://github.com/IRamNoc/Portfolio',
         detailIsDisplayed: false,
     },
     {
-        id: "riddle",
-        name: "Transfer Riddle",
-        technologies: 'Java, J2EE(Web Services), JavaScript, Html5, CSS, BootStrap',
+        id: "",
+        name: "",
+        technologies: '',
         images: [
             {
-                title: 'riddle',
-                src: "assets/images/riddle.png",
+                title: '',
+                src: "",
             },
         ],
-        url: 'https://transfer-riddle-solver.herokuapp.com/index.html',
-        detail: "Affichage des étapes de la résolution d'énigme de transvasement. Étant donné deux réservoirs vides de contenance X et Y, comment obtenir précisément T litres dans l’un des deux réservoirs (T étant un entier). On peut remplir ou vider un réservoir, et transvaser le contenu de l’un dans l’autre, jusqu’à ce que la source soit vide ou la destination pleine.",
-        codeSourceUrl: 'https://github.com/zinedineBenkhider/transfer_riddle_client',
+        url: '',
+        detail: "",
+        codeSourceUrl: '',
+        detailIsDisplayed: false,
+    },
+    {
+        id: "",
+        name: "",
+        technologies: '',
+        images: [
+            {
+                title: '',
+                src: "",
+            },
+        ],
+        url: '',
+        detail: "",
+        codeSourceUrl: '',
+        detailIsDisplayed: false,
+    },
+    {
+        id: "",
+        name: "",
+        technologies: '',
+        images: [
+            {
+                title: '',
+                src: "",
+            },
+        ],
+        url: '',
+        detail: "",
+        codeSourceUrl: '',
+        detailIsDisplayed: false,
+    },
+    {
+        id: "",
+        name: "",
+        technologies: '',
+        images: [
+            {
+                title: '',
+                src: "",
+            },
+        ],
+        url: '',
+        detail: "",
+        codeSourceUrl: '',
         detailIsDisplayed: false,
     },
 ];
